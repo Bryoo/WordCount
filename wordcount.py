8f21d14844475027ad2249a8fbdeb4dc87e07be5
Bryoo <bryantskisseu@gmail.com>
2017-05-10 07:41:49 +0300

    final numeic test passed

--- wordcount.py
+++ wordcount.py
@@ -2,15 +2,25 @@ from collections import Counter
 
 
 def words(string):
-    word_list = string.split()
+    word_list1 = string.split()
+    word_list = []
+    for elem in word_list1:
+        try:
+            word_list.append(int(elem))
+        except Exception:
+            word_list.append(elem)
+
     word_dict = dict(Counter(word_list))
     return word_dict
+
 
 dady = "i love love being t t"
 mom = dady.split()
 print(mom)
 
-my_dict = words("eye eye list list you you")
+my_dict = words("4 eye 21 list you you")
 print(my_dict)
+
+
 
 
